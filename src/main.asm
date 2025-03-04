90d530b1adc7cade2589c7ef280ad84ae14ad699
jscheel6502 <jacob@escheel.com>
2025-03-03 22:14:44 -0600

    Update src

--- src/main.asm
+++ src/main.asm
@@ -1,60 +1,48 @@
 .setcpu "65C02"
 
-VIA_PORTA = $4001
-VIA_DDRA = $4003
-
-ACIA_DATA = $5000
-ACIA_STATUS = $5001
-ACIA_COMMAND = $5002
-ACIA_CONTROL = $5003
-
+;===============================================================================
 .segment "START"
 RESET:
     sei             ; Disable Interrupts.
     ldx #$ff
     txs             ; Initialize the stack pointer.
-    lda #$ff
-    sta VIA_DDRA    ; Set all pins in VIA_PORTA to output.
-    stz ACIA_STATUS ; Signal acia chip to reset itself.
-    lda #%00011111  ; 1sb-8wl-buad-19200
-    sta ACIA_CONTROL
-    lda #%10001011  ; No parity, no echo, ints disabled, DTR
-    sta ACIA_COMMAND
+    jsr VIA_INIT
+    jsr ACIA_INIT
+    ;cli            ; Enable Interrupts.
 
+;===============================================================================
 MAIN:
-    ldx #0
-LOOP:
-    lda teststr,x
-    beq MAIN_LOOP
-    jsr ACIA_SEND
-    inx
-    jmp LOOP
+    lda #%10101010
+    sta VIA_PORTA
 MAIN_LOOP:
+    jsr ACIA_RECV
     jmp MAIN_LOOP
 
-ACIA_SEND:
-    pha
-    phx
-    ldx #0
-TX_DELAY:
-    inx
-    txa
-    cmp #100
-    bne TX_DELAY
-    plx
-    pla
-    sta ACIA_DATA
-    rts
-
+;===============================================================================
 HALT:
     sei
+    ldx #$0
+HALT_PRINTS:
+    lda str_halt,x
+    beq HALT_LOOP
+    jsr ACIA_SEND
+    inx
+    jmp HALT_PRINTS
 HALT_LOOP:
-    jmp HALT
+    nop
+    jmp HALT_LOOP
 
-teststr: 
-    .byte "This is a test string ..."
+;===============================================================================
+.include "via.inc"
+.include "acia.inc"
+.include "lib.inc"
+
+;===============================================================================
+str_halt:
+    .byte "System Halted ..."
     .byte $00
 
+;===============================================================================
 .segment "VECTORS"
     .word $0000
     .word RESET
