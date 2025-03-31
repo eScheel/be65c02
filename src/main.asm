61546b01ceadb760b5a8e42f7757590bb24e3740
jscheel6502 <jacob@escheel.com>
2025-03-30 22:50:55 -0500

    Update src.

--- src/main.asm
+++ src/main.asm
@@ -1,21 +1,24 @@
 .setcpu "65C02"
 
-; Zero Page Variables.
 ticks      = $00
 serial_in  = $01
 shift_in   = $02
 addr_lo    = $03
 addr_hi    = $04
 
+counter_in   = $500
+input_string = $501
+
 ;===============================================================================
 .segment "START"
 RESET:
     sei             ; Set interrupt disable status.
     cld             ; Clear decimal mode. 
-    ldx #$ff
+    ldx #$FF
     txs             ; Initialize the stack pointer.
     jsr VIA_INIT
     jsr ACIA_INIT
+    stz counter_in  ; Initialize the input counter.
     cli             ; Clear interrupt disable bit.
     jmp MAIN
 .include "irq.inc"
@@ -27,20 +30,119 @@ RESET:
 ;===============================================================================
 MAIN:
     ldy #0
-PRINTS:
+PRINTS_HELLO:
     lda str_hello,y
-    beq MAIN_LOOP
+    beq PROMPT
     jsr ACIA_PRINTC
     iny
-    jmp PRINTS
-MAIN_LOOP:
-    lda serial_in
-    beq MAIN_LOOP
+    jmp PRINTS_HELLO
+PROMPT:
+    lda #'>'
     jsr ACIA_PRINTC
-    stz serial_in
+MAIN_LOOP:
+    jsr RETURN_PULL     ; io.inc
     jmp MAIN_LOOP
 
 ;===============================================================================
+RETURN_PULL:
+    lda #$0D
+    cmp serial_in
+    beq PROCESS_INPUT
+    rts
+
+;===============================================================================
+PROCESS_INPUT:
+    sei
+    pha
+    phx
+    jsr ACIA_PRINTNL
+    jsr PARSE_CMD    
+PROCESS_DONE:
+    stz serial_in
+    stz counter_in
+    jsr ACIA_PRINTNL
+    lda #'>'
+    jsr ACIA_PRINTC
+    plx
+    pla
+    cli
+    jmp MAIN_LOOP
+
+;===============================================================================
+PARSE_CMD:
+    pha
+    phx
+    ldx #0
+PARSE_HELP:
+    lda str_help_cmd,X
+    beq HELP
+    cmp input_string,X
+    bne PARSE_DUMP
+    inx
+    jmp PARSE_HELP
+PARSE_DUMP:
+    ldx #0
+PD_LOOP:
+    lda str_dump_cmd,X
+    beq DUMP
+    cmp input_string,X
+    bne PARSE_HALT
+    inx
+    jmp PD_LOOP
+PARSE_HALT:
+    ldx #0
+PH_LOOP:
+    lda str_halt_cmd,X
+    beq HALT
+    cmp input_string,X
+    bne PARSE_CMD_DONE
+    inx
+    jmp PH_LOOP
+PARSE_CMD_DONE:
+    plx
+    pla
+    rts
+
+HELP:
+    ldx #0
+PRINT_HELP:
+    lda str_help,X
+    beq PARSE_CMD_DONE
+    jsr ACIA_PRINTC
+    inx
+    jmp PRINT_HELP
+
+DUMP:
+    lda #'7'
+    sta value
+    lda #'0'
+    sta value + 1
+    jsr HEX_TO_BIN
+    lda conversion
+    sta value
+    jsr BIN_TO_BIN
+    lda #'#'
+    jsr ACIA_PRINTC
+    lda #'%'
+    jsr ACIA_PRINTC
+    lda conversion
+    jsr ACIA_PRINTC
+    lda conversion + 1
+    jsr ACIA_PRINTC
+    lda conversion + 2
+    jsr ACIA_PRINTC
+    lda conversion + 3
+    jsr ACIA_PRINTC
+    lda conversion + 4
+    jsr ACIA_PRINTC
+    lda conversion + 5
+    jsr ACIA_PRINTC
+    lda conversion + 6
+    jsr ACIA_PRINTC
+    lda conversion + 7
+    jsr ACIA_PRINTC
+    jmp PARSE_CMD_DONE
+
 HALT:
     jsr ACIA_PRINTNL
     sei                 ; Disable Interrupts.
@@ -60,8 +162,34 @@ str_hello:
     .byte $0D
     .byte $0A
     .byte $00
+str_help:
+    .byte "Possible Command:"
+    .byte $0D
+    .byte $0A
+    .byte "help - (Prints this message.)"
+    .byte $0D
+    .byte $0A
+    .byte "dump - (Dumps contents of memory.)"
+    .byte $0D
+    .byte $0A
+    .byte "halt - (Halts the CPU.)"
+    .byte $0D
+    .byte $0A
+    .byte $00
 str_halt:
     .byte "System Halted ..."
+    .byte $00
+str_addr:
+    .byte "ADDR >"
+    .byte $00
+str_help_cmd:
+    .byte "help"
+    .byte $00
+str_dump_cmd:
+    .byte "dump"
+    .byte $00
+str_halt_cmd:
+    .byte "halt"
     .byte $00
 
 ;===============================================================================
